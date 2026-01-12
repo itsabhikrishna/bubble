eff62118b84c7fe64b41d4b00d4efbdde4019438
itsabhikrishna <krishnaabhi1510@gmail.com>
2026-01-12 12:27:20 +0530

    Updates

---
+++ _next/static/chunks/2688ed09fd1d2872.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/422e4584bc52dcec.js",
+  "static/chunks/turbopack-d04f856cde4caebf.js"
+])
---
+++ _next/static/plr8IAsPu98ILMCCbiJGz/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/2688ed09fd1d2872.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/bubble/_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
